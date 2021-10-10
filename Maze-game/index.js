bc5f1733d9222271699ddafe71f76f0baa515ad3
GeorgeOrzanescu <george.asro@gmail.com>
2021-10-10 19:58:58 +0300

    Added walls and grid

--- Maze-game/index.js
+++ Maze-game/index.js
@@ -9,21 +9,42 @@ const { Engine, Composite, Render, Bodies, Runner } = Matter;
 const engine = Engine.create();
 const world = engine.world; // world is created by the engine
 
+const width = 800;
+const height = 800;
+
 const render = Render.create({
   element: document.body, // where to create the render
   engine: engine, // what engine to use
   options: {
-    width: 800,
-    height: 800,
+    width: width,
+    height: height,
     wireframes: false, // default is true bodies don't fall
   },
 });
+
+//create border around the world
+const walls = [
+  Bodies.rectangle(width / 2, 0, width, 10, { isStatic: true }), // upper
+  Bodies.rectangle(width / 2, height, width, 10, { isStatic: true }), // lower
+  Bodies.rectangle(width, height / 2, 10, height, { isStatic: true }), // right
+  Bodies.rectangle(0, height / 2, 10, height, { isStatic: true }), // left
+];
 
 // create bodies
 const box = Bodies.rectangle(200, 200, 40, 40);
 const circle = Bodies.circle(400, 400, 20);
 
 Composite.add(world, [box, circle]); // add bodies
+Composite.add(world, walls); // add walls
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
+
+const cellsVertical = 4;
+const cellsHorizontal = 4;
+
+const grid = Array(cellsVertical) // create array for each cell of the grid
+  .fill(null)
+  .map(() => Array(cellsHorizontal).fill(true));
+
+console.log(grid);
